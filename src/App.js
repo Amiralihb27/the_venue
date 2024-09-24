b6738d4fc6b29da062b2eaa61bbed593bcb8a84e
Amirali <amiralihabibi82@gmail.com>
2024-09-24 23:24:26 +0330

    Add a carousel to the webpage

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import './resources/styles.css';
 import Header from "./components/header-footer/Header";
 import Footer from "./components/header-footer/Footer";
+import Featured from "./components/featured";
 const App = () => {
   return (
     <div className="App">
       <Header></Header>
+      <Featured />
       <div style={{backgroundColor:'red' , height:'800px'}}></div>
       <div style={{backgroundColor:'blue' , height:'800px'}}></div>
       <div style={{backgroundColor:'green' , height:'800px'}}></div>
