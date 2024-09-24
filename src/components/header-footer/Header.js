ffd52d1e6b9b694383d3c2594ad39640f609d22b
Amirali <amiralihabibi82@gmail.com>
2024-09-24 21:40:31 +0330

    Update the header

--- src/components/header-footer/Header.js
+++ src/components/header-footer/Header.js
@@ -2,32 +2,45 @@ import React, { useState, useEffect } from 'react';
 
 import { AppBar, Toolbar, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import SideDrawer from "./SideDrawer";
+import SideDrawer from './SideDrawer';
 
 const Header = () => {
 
-    const [drawerOpen, setDrawerOpen] = useState(false)
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    const [showHeader,setShowHeader]= useState(false);
 
     const  toggleDrawer = (value)=>{
         setDrawerOpen(value);
     } 
+
+    const handleScroll = ()=>{
+       if(window.scrollY>0){
+        setShowHeader(true);
+       }else{
+        setShowHeader(false)
+       }
+    }
+
+    useEffect(()=>{
+        window.addEventListener('scroll',handleScroll)
+    },[])
     return (
         <AppBar
             position='fixed'
             style={{
-                backgroundColor: '#2f2f2f',
+                backgroundColor:showHeader ? '#2f2f2f' : 'transparent',
                 boxShadow: 'none',
                 padding: '10px 0px'
             }}>
 
             <Toolbar>
                 <div className='header_logo'>
-                    <div className='font_righ header_logo_venu'>The Venu</div>
+                    <div className='font_righteous header_logo_venue'>The Venu</div>
                     <div className='header_logo_title'>Musical Events</div>
 
                 </div>
                 <IconButton
-                    aria-lable="Menu"
+                    aria-label="Menu"
                     color='inherit'
                     onClick={()=> toggleDrawer(true)}>  
                     <MenuIcon />
--- src/components/header-footer/SideDrawer.js
+++ src/components/header-footer/SideDrawer.js
@@ -5,23 +5,23 @@ import { Draw } from '@mui/icons-material';
 
 const SideDrawer = (props) => {
 
-const listItems=[
-    {where:'featured' , value:'to The top'},
-    {where:'venuInfo' , value :'Venue Info'},
-    {where:'highlights' , value:'Highlights'},
-    {where:'location' , value:'Location'}
-]
+    const listItems = [
+        { where: 'featured', value: 'to The top' },
+        { where: 'venuInfo', value: 'Venue Info' },
+        { where: 'highlights', value: 'Highlights' },
+        { where: 'location', value: 'Location' }
+    ]
 
-function renderItems(item){
-    return <ListItem button onClick={() => alert(item.where)}>{item.value} </ListItem>
-}
+    function renderItems(item) {
+        return <ListItem button onClick={() => alert(item.value)} key={item.where}>{item.value} </ListItem>
+    }
     return (
         <Drawer
             anchor='right'
             open={props.open}
-            onClose={()=> props.close(false)}>
+            onClose={() => props.close(false)}>
             <List component="nav">
-              {  listItems.map((item)=>renderItems(item))}
+                {listItems.map((item) => renderItems(item))}
             </List>
 
         </Drawer>
